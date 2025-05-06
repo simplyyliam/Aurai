8dde36d6aeca038159a51a96bc82c606f867338d
sipllyyliam <simplyliam.dev@gmail.com>
2025-05-06 12:05:59 +0200

    feat: enhance Card and Home components with GSAP animations

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useRef } from "react";
 import Card from "../components/Card"
+import gsap from "gsap";
 
 
 function Home() {
+
+  const TitleRef = useRef(null)
+
+
+  useEffect (() => {
+    if (TitleRef) {
+      gsap.to(TitleRef.current, {
+        opacity: 1,
+        scale: 1,
+        duration: 1.2,
+        ease: "expo.out"
+      })
+    }
+  })
+
   return (
     <>
-      <div className="flex items-center justify-center w-screen h-screen bg-background text-white overflow-hidden ">
-        <h1 className="text-header italic font-dm-sans font-light  tracking-header">
+      <div className="flex items-center justify-center w-screen h-screen bg-background text-white overflow-hidden">
+        <h1
+          ref={TitleRef}
+          className="text-header italic font-dm-sans font-light  tracking-header opacity-0 scale-0"
+        >
           Aurai
         </h1>
         <Card
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import gsap from "gsap";
 
 interface Props {
     Title: string;
@@ -10,11 +12,34 @@ interface Props {
     left?: number
     right?: number
     link: string
+    // Ref?: React.Ref<HTMLButtonElement>
 }
 
 function Card({Title, Background, Width, Height, top, bottom, left, right, link}:Props) {
+
+  const CardRef = useRef(null)
+  useEffect(() => {
+    if (CardRef.current) {
+      gsap.fromTo(
+        CardRef.current,
+        { opacity: 0, scale: 0 },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 0.5,
+          ease: "power3.out",
+          stagger: {
+            amount: 0.5, // Total time for the stagger effect
+            from: "start", // Animation starts from the first element
+          },
+        }
+      );
+    }
+  }, []);
+
 return (
   <button
+    ref={CardRef}
     style={{
       width: `${Width}px`,
       height: `${Height}px`,
@@ -23,21 +48,21 @@ return (
       left: `${left}em`,
       right: `${right}em`,
     }}
-    className="absolute rounded-3xl flex items-center justify-center bg-amber-50 transition-transform ease-linear duration-200 hover:scale-[1.02] "
-    >
+    className="absolute rounded-3xl flex items-center justify-center opacity-0 scale-0 bg-amber-50 transition-transform ease-linear duration-200 hover:scale-[1.02]"
+  >
     <div className="relative w-full h-full overflow-hidden">
       <img
-      className="absolute w-full h-full object-cover rounded-2xl transition-transform ease-linear duration-200 hover:scale-[0.5]"
-      src={Background}
-      alt="Card background image"
+        className="absolute w-full h-full object-cover rounded-2xl transition-transform ease-linear duration-200 hover:scale-[0.5]"
+        src={Background}
+        alt="Card background image"
       />
       <Link to={link}>
         <h1 className="absolute inset-0 flex items-center justify-center text-2xl font-dm-sans transition-transform ease-linear duration-200 hover:scale-[1.1]">
-        {Title}
+          {Title}
         </h1>
       </Link>
     </div>
-    </button>
+  </button>
 );
 }
 
